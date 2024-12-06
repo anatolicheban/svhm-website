7fe78b2ee166dd503e2c9cc2aeed9a736af42d34
Sashas <sasaspychka@gmail.com>
2024-12-06 19:03:08 +0200

    new-section

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,13 @@
 import '../styles/style.scss';
+
+document.querySelectorAll('.questions-box-item').forEach(item => {
+    item.addEventListener('click', () => {
+        document.querySelectorAll('.questions-box-item').forEach(otherItem => {
+            if (otherItem !== item) {
+                otherItem.classList.remove('active');
+            }
+        });
+
+        item.classList.toggle('active');
+    });
+});
