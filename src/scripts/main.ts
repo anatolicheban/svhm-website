c4a1c896a75206b93ecc248b1a6a274f51e4944c
Sashas <sasaspychka@gmail.com>
2024-12-07 16:44:16 +0200

    new-page

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -11,3 +11,15 @@ document.querySelectorAll('.questions-box-item').forEach(item => {
         item.classList.toggle('active');
     });
 });
+
+document.querySelectorAll('.faq-box-item').forEach(item => {
+    item.addEventListener('click', () => {
+        document.querySelectorAll('.faq-box-item').forEach(otherItem => {
+            if (otherItem !== item) {
+                otherItem.classList.remove('active');
+            }
+        });
+
+        item.classList.toggle('active');
+    });
+});
