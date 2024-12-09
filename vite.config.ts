386d8498e3565e2a3dcc7ece210b0107dd3d9180
Sashas <sasaspychka@gmail.com>
2024-12-09 23:11:04 +0200

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,11 @@ export default defineConfig({
       input: {
         index: resolve(__dirname, "index.html"),
         ["all-courses"]: resolve(__dirname, "all-courses.html"),
-        about: resolve(__dirname, "about-page.html"),
+        about: resolve(__dirname, "about.html"),
+        blogs: resolve(__dirname, "blogs.html"),
+        article: resolve(__dirname, "article.html"),
+        course: resolve(__dirname, "course.html"),
+        event: resolve(__dirname, "event.html"),
       },
     },
   },
