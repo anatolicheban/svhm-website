b83a9b5c970efa61f491e9cf986b5a2d33617779
Anatoli Cheban <107035461+anatolicheban@users.noreply.github.com>
2024-12-09 22:32:20 +0200

    temp

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,29 +1,29 @@
-import '../styles/style.scss';
+import "../styles/style.scss";
 
-document.querySelectorAll('.questions-box-item').forEach(item => {
-    item.addEventListener('click', () => {
-        document.querySelectorAll('.questions-box-item').forEach(otherItem => {
-            if (otherItem !== item) {
-                otherItem.classList.remove('active');
-            }
-        });
-
-        item.classList.toggle('active');
+document.querySelectorAll(".questions-box-item").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".questions-box-item").forEach((otherItem) => {
+      if (otherItem !== item) {
+        otherItem.classList.remove("active");
+      }
     });
+
+    item.classList.toggle("active");
+  });
 });
 
-document.querySelectorAll('.faq-box-item').forEach(item => {
-    item.addEventListener('click', () => {
-        document.querySelectorAll('.faq-box-item').forEach(otherItem => {
-            if (otherItem !== item) {
-                otherItem.classList.remove('active');
-            }
-        });
-
-        item.classList.toggle('active');
+document.querySelectorAll(".faq-box-item").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".faq-box-item").forEach((otherItem) => {
+      if (otherItem !== item) {
+        otherItem.classList.remove("active");
+      }
     });
+
+    item.classList.toggle("active");
+  });
 });
 
-window.addEventListener('load', () => {
-    document.body.classList.remove('transition-disabled');
+window.addEventListener("load", () => {
+  document.body.classList.remove("transition-disabled");
 });
