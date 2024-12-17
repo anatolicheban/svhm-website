34c6170f1ed664fae5be0b3d1c677a8aac56a7d5
Sashas <sasaspychka@gmail.com>
2024-12-17 21:14:04 +0200

    adaptive

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -28,6 +28,23 @@ window.addEventListener("load", () => {
   document.body.classList.remove("transition-disabled");
 });
 
+document.querySelectorAll(".conditions-box-item").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".conditions-box-item").forEach((otherItem) => {
+      if (otherItem !== item) {
+        otherItem.classList.remove("active");
+      }
+    });
+
+    item.classList.toggle("active");
+  });
+});
+
+window.addEventListener("load", () => {
+  document.body.classList.remove("transition-disabled");
+});
+
+
 // HAMBURGER
 
 document.addEventListener('DOMContentLoaded', () => {
