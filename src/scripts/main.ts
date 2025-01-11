b86dc754c63c803c0b7a0aaa249006245177c134
Sashas <sasaspychka@gmail.com>
2025-01-11 16:38:07 +0200

    botik

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -90,8 +90,8 @@ const name = document.getElementById('name') as HTMLInputElement;
 const phone = document.getElementById('phone') as HTMLInputElement;
 const question = document.getElementById('question') as HTMLTextAreaElement;
 const messageBox = document.querySelector('.contacts-message-box') as HTMLElement;
-const courseTitleElement = document.querySelector('.hero-section-info__title') as HTMLElement || null;
-const courseTitle = courseTitleElement ? courseTitleElement.textContent.trim() : '';
+const courseTitleElement = document.querySelector('.hero-section-info__title') as HTMLElement
+const courseTitle = courseTitleElement ? courseTitleElement!.textContent.trim() : '';
 
 function showMessage(message: string, type: 'error' | 'success'): void {
   const messageElement = document.createElement('div');
